ef0a9a4a93fa6bcafc37029ddca3c8afedb17d2c
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-01-18 16:30:01 +0000

    uppl message

--- clean_messages_raw_file.py
+++ clean_messages_raw_file.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 def clean_messages(df):
-  # удаляем строки, в которых нет данных в поле СистСтатус ЗаказТОРО
+  
   result_df = df
+  # удаляем строки, в которых нет данных в поле СистСтатус ЗаказТОРО
   result_df.dropna(subset=['СистСтатус ЗаказТОРО', ], inplace=True)
+  # удаляем колонку Код ABC
+  result_df.drop(['Код ABC', 'Номер адреса', 'ОснСредство'], inplace=True, axis=1)
   return result_df
